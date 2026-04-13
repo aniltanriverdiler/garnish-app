42be83b1102529dfc9e922322f0d8d9d56d0bd18
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2026-04-13 13:02:22 +0300

    Update project structure with new components, styles, and assets; add Tailwind CSS utilities and custom fonts; update dependencies including nativewind to version 4.2.1.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,25 @@
+import { SplashScreen, Stack } from 'expo-router';
 import '../global.css';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
-import { Stack } from 'expo-router';
+export default function TabLayout() {
+  const [fontsLoaded, error] = useFonts({
+    'Quicksand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
+    'Quicksand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
+    'Quicksand-Regular': require('../assets/fonts/Quicksand-Regular.ttf'),
+    'Quicksand-SemiBold': require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    'Quicksand-Light': require('../assets/fonts/Quicksand-Light.ttf'),
+  });
 
-export default function Layout() {
-  return (
-    <SafeAreaProvider>
-      <Stack />
-    </SafeAreaProvider>
-  );
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
