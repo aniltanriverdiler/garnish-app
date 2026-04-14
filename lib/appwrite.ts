52fdcc30408baf915653bda40628a166da455d86
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2026-04-14 16:22:48 +0300

    feat(auth): add getCurrentUser function to retrieve current user data
    - Implemented getCurrentUser function to fetch the current user's document from the database
    - Enhanced error handling for user retrieval process

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,5 @@
-import {Account, Avatars, Client, Databases, ID, Query, Storage} from "react-native-appwrite";
-import {CreateUserParams, GetMenuParams, SignInParams} from "@/type";
+import { Account, Avatars, Client, Databases, ID, Query, Storage } from 'react-native-appwrite';
+import { CreateUserParams, GetMenuParams, SignInParams } from '@/type';
 
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
@@ -46,5 +46,25 @@ export const signIn = async ({ email, password }: SignInParams) => {
     const session = await account.createEmailPasswordSession(email, password);
   } catch (e) {
     throw new Error(e as string);
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get();
+    if (!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal('accountId', currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+    throw new Error(error as string);
   }
 };
